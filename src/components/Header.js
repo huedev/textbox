fa339b6996d344505d1de09cdc420990139255b7
hue <32443158+huedev@users.noreply.github.com>
2022-09-12 02:28:29 -0500

    Layout tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import { WrenchScrewdriverIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
   return (
-    <header className="bg-indigo-500 px-4 py-2 mb-8">
-      <div className="max-w-7xl mx-auto">
+    <header className="bg-indigo-500 px-4 py-3 mb-8">
+      <div className="max-w-6xl mx-auto">
         <div className="flex">
           <a href="/" className="flex items-center text-2xl text-white">
             <WrenchScrewdriverIcon className="w-6 h-6 mr-1" />

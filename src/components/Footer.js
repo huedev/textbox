b1a755a7e6522473f34540e0de2f08cbe2563c8c
hue <32443158+huedev@users.noreply.github.com>
2022-09-13 01:23:28 -0500

    Minor layout tweaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="px-4 text-slate-600">
       <div className="border-t border-slate-100 max-w-6xl mx-auto py-4">
         <div className="flex justify-between">
-          <span>&copy; huedev</span>
+          <span>&copy; 2022 huedev</span>
           <Link to="/about" className="hover:underline">About</Link>
         </div>
       </div>

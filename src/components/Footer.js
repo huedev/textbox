3860e3a13d2fd5a9f253909ee1468d97a9d345d5
hue <32443158+huedev@users.noreply.github.com>
2022-09-19 15:34:30 -0500

    GitHub link in footer, style changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid'
 
 const Footer = () => {
   return (
@@ -6,7 +7,15 @@ const Footer = () => {
       <div className="border-t border-slate-300 dark:border-slate-700 max-w-5xl mx-auto py-6">
         <div className="flex justify-between">
           <span>&copy; 2022 huedev</span>
-          <Link to="/about" className="hover:text-slate-700 dark:hover:text-slate-300">About</Link>
+          <nav className="flex space-x-4 text-indigo-600 dark:text-indigo-400">
+            <Link to="/about" className="hover:text-indigo-700 dark:hover:text-indigo-300">
+              About
+            </Link>
+            <a href="https://github.com/huedev/textbox" target="_blank" rel="noreferrer" className="flex items-center hover:text-indigo-700 dark:hover:text-indigo-300">
+              GitHub
+              <ArrowTopRightOnSquareIcon className="ml-1 h-5 w-5" aria-hidden="true" />
+            </a>
+          </nav>
         </div>
       </div>
     </footer>

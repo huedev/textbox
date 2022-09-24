4c15c74deef9b9bbc10f9b15259667d736fa1ee5
hue <32443158+huedev@users.noreply.github.com>
2022-09-24 13:18:04 -0500

    Add name property to Switch

--- src/components/MySwitch.js
+++ src/components/MySwitch.js
@@ -7,6 +7,7 @@ const MySwitch = (props) => {
         <Switch
           checked={props.currentValue}
           onChange={event => props.handleChange(event, props.name)}
+          name={props.name}
           className={`${props.currentValue ? 'bg-indigo-500 dark:bg-indigo-700' : 'bg-slate-400 dark:bg-slate-700'}
             relative inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
         >
